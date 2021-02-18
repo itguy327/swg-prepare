750b29303040b23d4611daddf4604b20f97a28f4
Jason <itguy327@gmail.com>
2021-02-17 20:59:47 -0500

    LXC Mods

--- oci8.sh
+++ oci8.sh
@@ -5,12 +5,12 @@ apt-get update
 apt-get install alien php-dev php-pear libaio1 build-essential -y
 
 # Get the instantclients
-wget https://github.com/tekaohswg/gdown.pl/archive/v1.4.zip
-unzip v1.4.zip
-rm v1.4.zip
-./gdown.pl-1.4/gdown.pl 'https://drive.google.com/open?id=1PFtRlatlozfairdclfHI-46CwaVGQAb-' 'oracle-instantclient18.5-basic-18.5.0.0.0-3.x86_64.rpm'
-./gdown.pl-1.4/gdown.pl 'https://drive.google.com/open?id=15NXyoE2eaOAQoO3c3Ttp87HBR5hWBN4G' 'oracle-instantclient18.5-devel-18.5.0.0.0-3.x86_64.rpm'
-rm -r gdown.pl-1.4
+#wget https://github.com/tekaohswg/gdown.pl/archive/v1.4.zip
+#unzip v1.4.zip
+#rm v1.4.zip
+perl gdown.pl  'https://drive.google.com/open?id=1PFtRlatlozfairdclfHI-46CwaVGQAb-' 'oracle-instantclient18.5-basic-18.5.0.0.0-3.x86_64.rpm'
+perl gdown.pl  'https://drive.google.com/open?id=15NXyoE2eaOAQoO3c3Ttp87HBR5hWBN4G' 'oracle-instantclient18.5-devel-18.5.0.0.0-3.x86_64.rpm'
+#rm -r gdown.pl-1.4
 
 # Install the instantclients
 alien -i oracle-instantclient18.5-basic-18.5.0.0.0-3.x86_64.rpm
